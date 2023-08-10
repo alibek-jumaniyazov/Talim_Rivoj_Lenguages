2ac6c72a6d27e760ba0864e5403c0b742ffbeb0a
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2023-08-10 13:52:28 +0500

    telegram Bot add Fuunction

--- talim-rivoj-languages/src/App.jsx
+++ talim-rivoj-languages/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import TelegramBotForm from './TelegramBotForm.jsx'
 
 function App() {
 
   return (
    <div className="App">
-
+        <TelegramBotForm/>
    </div>
   )
 }
---
+++ talim-rivoj-languages/src/TelegramBotForm.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const TelegramBotForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    tel: '',
+    til: '',
+  });
+
+  const [til , setTil] = useState("Turk")
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    //  'YOUR_BOT_API_TOKEN' Bu yerga Telegram bot token API yoziladi
+    const apiToken = '6406627293:AAH7VNgJ9r1Y6tqLjBpC7C07VVvtQLeHQ08';
+    const chatId = '2119173106'; // Telegram user ni ChatId 
+
+    const message = `
+      Yengi foydalanuvchi:
+      Name: ${formData.name}
+      Telefon Raqam: ${formData.tel}
+      Til: ${til}
+    `;
+
+    try {
+      await axios.post(`https://api.telegram.org/bot${apiToken}/sendMessage`, {
+        chat_id: chatId,
+        text: message,
+      });
+
+      // Malumotlarni uzatganizdan keyin input ishidegi malumot ochib ketadi
+      setFormData({ name: '', tel: '', setTil: 'Turk' });
+    } catch (error) {
+      console.error('Error sending message to Telegram:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="name" placeholder="Ism Familiya" value={formData.name} onChange={handleChange} />
+      <input type="tel" name="tel" placeholder="Telefon raqam" value={formData.tel} onChange={handleChange} />
+      {/* <textarea name="message" placeholder="Message" value={formData.message} onChange={handleChange} /> */}
+      <div className="">
+
+        <h5 href='#' onClick={() => setTil( 'Turk')}>turk</h5>
+        <h5 href='#' onClick={() => setTil( 'Engilsh')}>Engilsh</h5>
+        <h5 href='#' onClick={() => setTil( 'Rus')}>Rus</h5>
+
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default TelegramBotForm;
