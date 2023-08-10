f28eb1cff320c17a77788aa236dcba067c39017e
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2023-08-10 14:00:36 +0500

    add

--- talim-rivoj-languages/src/TelegramBotForm.jsx
+++ talim-rivoj-languages/src/TelegramBotForm.jsx
@@ -8,7 +8,7 @@ const TelegramBotForm = () => {
     til: '',
   });
 
-  const [til , setTil] = useState("Turk")
+  const [til, setTil] = useState("Turk")
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -30,13 +30,20 @@ const TelegramBotForm = () => {
     `;
 
     try {
-      await axios.post(`https://api.telegram.org/bot${apiToken}/sendMessage`, {
-        chat_id: chatId,
-        text: message,
-      });
+      if (formData.name == '', formData.tel == '') {
+        alert('iltimos Hamma joylarni toldiring')
+      }
+      else {
+        await axios.post(`https://api.telegram.org/bot${apiToken}/sendMessage`, {
+          chat_id: chatId,
+          text: message,
+        });
 
-      // Malumotlarni uzatganizdan keyin input ishidegi malumot ochib ketadi
-      setFormData({ name: '', tel: '', setTil: 'Turk' });
+        // Malumotlarni uzatganizdan keyin input ishidegi malumot ochib ketadi
+        setFormData({ name: '', tel: '', setTil: 'Turk' });
+      }
+
+
     } catch (error) {
       console.error('Error sending message to Telegram:', error);
     }
@@ -49,9 +56,9 @@ const TelegramBotForm = () => {
       {/* <textarea name="message" placeholder="Message" value={formData.message} onChange={handleChange} /> */}
       <div className="">
 
-        <h5 href='#' onClick={() => setTil( 'Turk')}>turk</h5>
-        <h5 href='#' onClick={() => setTil( 'Engilsh')}>Engilsh</h5>
-        <h5 href='#' onClick={() => setTil( 'Rus')}>Rus</h5>
+        <h5 href='#' onClick={() => setTil('Turk')}>turk</h5>
+        <h5 href='#' onClick={() => setTil('Engilsh')}>Engilsh</h5>
+        <h5 href='#' onClick={() => setTil('Rus')}>Rus</h5>
 
       </div>
       <button type="submit">Submit</button>
